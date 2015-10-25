a3881061b0c063ad05f9d5561c38cdc98213b521
Aiden <Aiden@aidens-mbp>
2015-10-25 16:02:27 -0700

    commit

--- GIT/src/Layout.java
+++ GIT/src/Layout.java
@@ -7,60 +7,159 @@ import javax.swing.*;
 
 public class Layout extends JFrame {
 
+	JLabel sundaywidget;
 	JLabel mondaywidget;
+	JLabel tuesdaywidget;
+	JLabel wednesdaywidget;
+	JLabel thursdaywidget;
+	JLabel fridaywidget;
+	JLabel saturdaywidget;
+	
 	
 	public Layout(String title) {
 		super(title);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		initializeDockingComponents();
 
+		setLayout(new BorderLayout());
+		add(buildMiGDashboard(), BorderLayout.NORTH);
+		add(buildDatePanel(), BorderLayout.CENTER);
 		
-		add(buildMiGDashboard());
-		
-//		if(title.startsWith("MiG")){
-//			add(buildMiGDashboard());
-//		} else if(title.startsWith("Flow")) {
-//			add(buildMiGFlowLayout());
-//		} else {
-//			add(buildStandardDashboard());
-//		}
 		pack();
 
 	}
+	
+	JLabel datewidget;
+	
+	private JPanel buildDatePanel(){
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		panel.setPreferredSize(new Dimension(400, 400));
+		for (int b = 0; b<30; b++){
+			JPanel datepanel = new JPanel();
+			panel.add(datepanel);
+			datepanel.add(datewidget);
+			datewidget.setPreferredSize(new Dimension(80,100));
+			buildMigForm(datepanel);
+			
+			datepanel = new JPanel();
+			datepanel.add(new JButton("hi"));
+			datepanel.setBackground(Color.white);
+			datepanel.add(datewidget);
+			datepanel.setPreferredSize(new Dimension(75,100));
+			panel.add(datepanel);
+			
+			for (int a = 0; a<30; a++){
+				
+			}
+			
+		}		
+		return panel;
+		
+		
+	}
+	
 	private JPanel buildMiGDashboard() {
 		JPanel panel = new JPanel();
-//		JPanel sundaypanel = new JPanel();
+		JPanel sundaypanel = new JPanel();
 		JPanel mondaypanel = new JPanel();
-		mondaypanel.add(mondaywidget);  
-		mondaywidget.setPreferredSize(new Dimension(80, 200)); 
+		JPanel tuesdaypanel = new JPanel();
+		JPanel wednesdaypanel = new JPanel();
+		JPanel thursdaypanel = new JPanel();
+		JPanel fridaypanel = new JPanel();
+		JPanel saturdaypanel = new JPanel();
+//		sundaypanel.add(sundaywidget);
+//		mondaypanel.add(mondaywidget);  
+//		tuesdaypanel.add(tuesdaywidget);
+//		wednesdaypanel.add(wednesdaywidget);
+//		thursdaypanel.add(thursdaywidget);
+//		fridaypanel.add(fridaywidget);
+//		saturdaypanel.add(saturdaywidget);
+		sundaywidget.setPreferredSize(new Dimension(80, 50));
+		mondaywidget.setPreferredSize(new Dimension(80, 50)); 
+		tuesdaywidget.setPreferredSize(new Dimension(80, 50));
+		wednesdaywidget.setPreferredSize(new Dimension(80, 50));
+		thursdaywidget.setPreferredSize(new Dimension(80, 50));
+		fridaywidget.setPreferredSize(new Dimension(80, 50));
+		saturdaywidget.setPreferredSize(new Dimension(80, 50));
 		panel.setLayout(new MigLayout());
 		
+		sundaypanel.add(sundaywidget, "0,0,0");
 		mondaypanel.add(mondaywidget, "0,0,0");
+		tuesdaypanel.add(tuesdaywidget, "0,0,0");
+		wednesdaypanel.add(wednesdaywidget, "0,0,0");
+		thursdaypanel.add(thursdaywidget, "0,0,0");
+		fridaypanel.add(fridaywidget, "0,0,0");
+		saturdaypanel.add(fridaywidget, "0,0,0");
 		 
 		buildMiGForm(panel);
 		
 		
 		
-//		panel.setLayout(new MigLayout());
-//		buildMigForm(sundaypanel);
+		panel.setLayout(new MigLayout());
+		buildMigForm(sundaypanel);
 		panel.setLayout(new MigLayout());
 		buildMigForm(mondaypanel);
+		panel.setLayout(new MigLayout());
+		buildMigForm(tuesdaypanel);
+		panel.setLayout(new MigLayout());
+		buildMigForm(wednesdaypanel);
+		panel.setLayout(new MigLayout());
+		buildMigForm(thursdaypanel);
+		panel.setLayout(new MigLayout());
+		buildMigForm(fridaypanel);
+		panel.setLayout(new MigLayout());
+		buildMigForm(saturdaypanel);
+		
 
-
-//		sundaypanel = new JPanel();
-//		sundaypanel.setBackground(Color.red);
-//		sundaypanel.setBounds(0, 0, 0, 0);
+		sundaypanel = new JPanel();
+		sundaypanel.setBackground(Color.white);
+		sundaypanel.add(sundaywidget);
+		sundaypanel.setBounds(0, 0, 0, 0);
+		
 		mondaypanel = new JPanel();
 		mondaypanel.setBackground(Color.white);
 		mondaypanel.add(mondaywidget);
 		mondaypanel.setBounds(100, 100, 100, 100);
-	
+		
+		tuesdaypanel = new JPanel();
+		tuesdaypanel.setBackground(Color.white);
+		tuesdaypanel.add(tuesdaywidget);
+		tuesdaypanel.setBounds(200, 200, 200, 200);
+		
+		wednesdaypanel = new JPanel();
+		wednesdaypanel.setBackground(Color.white);
+		wednesdaypanel.add(wednesdaywidget);
+		wednesdaypanel.setBounds(100, 100, 100, 100);
+		
+		thursdaypanel = new JPanel();
+		thursdaypanel.setBackground(Color.white);
+		thursdaypanel.add(thursdaywidget);
+		thursdaypanel.setBounds(100, 100, 100, 100);
+		
+		fridaypanel = new JPanel();
+		fridaypanel.setBackground(Color.white);
+		fridaypanel.add(fridaywidget);
+		fridaypanel.setBounds(100, 100, 100, 100);
+		
+		saturdaypanel = new JPanel();
+		saturdaypanel.setBackground(Color.white);
+		saturdaypanel.add(saturdaywidget);
+		saturdaypanel.setBounds(100, 100, 100, 100);
 
 		setSize(100, 100);
 		setLocation(0, 0);
 		setResizable(true);
 		setVisible(true);
 
+		panel.add(sundaypanel);
+		panel.add(mondaypanel);
+		panel.add(tuesdaypanel);
+		panel.add(wednesdaypanel);
+		panel.add(thursdaypanel);
+		panel.add(fridaypanel);
+		panel.add(saturdaypanel);
+		
 		return panel;
 
 	}
@@ -101,8 +200,29 @@ public class Layout extends JFrame {
 	 private void initializeDockingComponents() {
 	        Font labelFont = new Font("Arial",Font.BOLD, 14);
 
+	        sundaywidget = new JLabel("Sunday", SwingConstants.CENTER);
+	        setUpLabel(sundaywidget, new Color(250,250,250), Color.RED, labelFont);
+	        
 	        mondaywidget = new JLabel("Monday", SwingConstants.CENTER);
-	        setUpLabel(mondaywidget, new Color(209,234,249), Color.BLACK, labelFont);
+	        setUpLabel(mondaywidget, new Color(250,250,250), Color.BLACK, labelFont);
+	        
+	        tuesdaywidget = new JLabel("Tuesday", SwingConstants.CENTER);
+	        setUpLabel(tuesdaywidget, new Color(250,250,250), Color.BLACK, labelFont);
+	        
+	        wednesdaywidget = new JLabel("Wednesday", SwingConstants.CENTER);
+	        setUpLabel(wednesdaywidget, new Color(250,250,250), Color.BLACK, labelFont);
+	        
+	        thursdaywidget = new JLabel("Thursday", SwingConstants.CENTER);
+	        setUpLabel(thursdaywidget, new Color(250,250,250), Color.BLACK, labelFont);
+	        
+	        fridaywidget = new JLabel("Friday", SwingConstants.CENTER);
+	        setUpLabel(fridaywidget, new Color(250,250,250), Color.BLACK, labelFont);
+	        
+	        saturdaywidget = new JLabel("Saturday", SwingConstants.CENTER);
+	        setUpLabel(saturdaywidget, new Color(250,250,250), Color.BLACK, labelFont);
+	        
+	        datewidget = new JLabel("", SwingConstants.CENTER);
+	        setUpLabel(datewidget, new Color(250,250,250), Color.black, labelFont);
 	        
 	 }
 	 private void setUpLabel(JLabel label, Color bg, Color fg, Font f) {
